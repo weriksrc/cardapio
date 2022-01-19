28536fb7377ea24a93f25ec63aeee0f824e68f6a
weriks <ti15@viacometa.com.br>
2022-01-19 18:24:58 -0400

    feat: inicia template produto

---
+++ src/components/Produto.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-card>
+      <BaseCarosel />
+    </v-card>
+    <span>
+      {{ title }}
+    </span>
+    <div>
+      {{ description }}
+    </div>
+    <v-divider></v-divider>
+    <div>
+      <span>Adicionais</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import BaseCarosel from "./BaseCarosel.vue"
+export default {
+
+  components:{
+    BaseCarosel
+  },
+
+  data(){
+    return{
+      title: "",
+      description: "",
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
