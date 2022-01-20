304be29a6ac298b41af1ad30dcbc116376f80d15
weriks <ti15@viacometa.com.br>
2022-01-20 09:07:53 -0400

    feat: prepara estrutura das novas views

---
+++ src/modules/pedidos/views/AddProductCart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <CurrentCaroselProduct />
+    <CurrentDescriptionProduct />
+    <ListCurrentAdditionalProduct />
+  </div>
+</template>
+
+<script>
+import CurrentCaroselProduct from "../components/CurrentCaroselProduct"
+import CurrentDescriptionProduct from "../components/CurrentDescriptionProduct"
+import ListCurrentAdditionalProduct from "../components/CurrentDescriptionProduct"
+
+export default {
+
+  components: {
+    CurrentCaroselProduct,
+    CurrentDescriptionProduct,
+    ListCurrentAdditionalProduct
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/modules/pedidos/views/Cardapio.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <CurrentImageCategory />
+    <ListProdudo />
+  </div>
+</template>
+
+<script>
+import CurrentImageCategory from "../components/CurrentImageCategory"
+import ListProdudo from "../components/ListProdudo"
+export default {
+  components:{
+    CurrentImageCategory,
+    ListProdudo
+  }
+}
+</script>
+
+<style>
+
+</style>
