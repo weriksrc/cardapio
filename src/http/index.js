adadd5624c543d3620ca94e2c85b8d4949cfa29c
Lucas Mota <lucasmota.dev@gmail.com>
2022-01-21 11:00:58 -0400

    chore

--- src/http/index.js
+++ src/http/index.js
@@ -5,7 +5,7 @@ const http = axios.create({
   headers: {
     "Content-Type": "application/json",
     Authorization:
-      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTY0MjcyNDYyOSwiZXhwIjoxNjQyNzI4MjI5LCJuYmYiOjE2NDI3MjQ2MjksImp0aSI6IlFkaEtFSlByUHhLR1pIT0wiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.ns2ZUxo67UOmcKUr3B8gpDWuF073bD-SlY34fo410ZA",
+      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTY0Mjc3NzEzMywiZXhwIjoxNjQyNzgwNzMzLCJuYmYiOjE2NDI3NzcxMzMsImp0aSI6InR1VnNDUzF5NTB2bHM2RlIiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.CD-TzWm_BitZwPwZ7EShWiBPFT9Xd3mU6K9Csvu6JLU",
   },
 });
 
