74d975431ffbc7bef78053aee5860062b5f49fac
Lucas Mota <lucasmota.dev@gmail.com>
2022-01-20 21:03:12 -0400

    chore: to

--- src/modules/pedidos/components/CardCategoria.vue
+++ src/modules/pedidos/components/CardCategoria.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container fluid>
     <v-row dense>
-      <v-col cols="6" sm="6" md="4">
-        <v-card :loading="false" max-width="200">
+      <v-col v-for="item in items" :key="item.id" cols="6" sm="6" md="4">
+        <v-card
+          :loading="false"
+          max-width="200"
+          :to="`categorias/${item.id}/cardapios`"
+        >
           <template slot="progress">
             <v-progress-linear
               color="deep-purple"
@@ -13,28 +17,11 @@
 
           <v-img
             height="150"
-            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            lazy-src="https://picsum.photos/id/11/10/6"
+            :src="item.imagen_path"
           ></v-img>
         </v-card>
         <v-card-title class="mx-auto">Cafes</v-card-title>
-      </v-col>
-
-      <v-col cols="6" sm="6" md="4">
-        <v-card :loading="false" max-width="200">
-          <template slot="progress">
-            <v-progress-linear
-              color="deep-purple"
-              height="10"
-              indeterminate
-            ></v-progress-linear>
-          </template>
-
-          <v-img
-            height="150"
-            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-          ></v-img>
-        </v-card>
-        <v-card-title>Cafes</v-card-title>
       </v-col>
     </v-row>
   </v-container>
