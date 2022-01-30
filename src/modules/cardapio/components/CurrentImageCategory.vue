20c5bd608094403215891aeb45c2aed1ca763548
Lucas Mota <lucasmota.dev@gmail.com>
2022-01-30 16:25:20 -0400

    refact: imgam categoria lista produtos

--- src/modules/cardapio/components/CurrentImageCategory.vue
+++ src/modules/cardapio/components/CurrentImageCategory.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-parallax src="https://image.freepik.com/vetores-gratis/pagina-de-erro-404-nao-encontrada_114341-25.jpg" height="250">
-  </v-parallax>
+  <v-parallax :src="src" height="250"> </v-parallax>
 </template>
 
 <script>
