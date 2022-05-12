30a0e8808ef9a1d2dcc5fba7790c6ed02e8ab5c1
Lucas Mota <lucasmota.dev@gmail.com>
2022-05-11 22:59:52 -0400

    refact: time line pedidos

--- src/modules/pedidos/components/TimelinePedidosMesa.vue
+++ src/modules/pedidos/components/TimelinePedidosMesa.vue
@@ -27,50 +27,49 @@
         </v-container>
       </v-img>
     </v-card>
-    <v-card-text class="py-0">
-      <v-timeline align-top dense>
-        <template v-for="(pedido, index) in pedidos">
-          <v-timeline-item
-            :color="colorStatusPedido(pedido.status_pedido_id)"
-            :key="index"
-            small
-          >
-            <template x-small v-slot:icon>
-              <!-- <v-icon>mdi-account</v-icon> -->
-            </template>
-            <v-row class="pt-0">
-              <v-col cols="3">
-                <strong>n° {{ pedido.id }}</strong>
-              </v-col>
-              <v-col>
-                <strong>{{ pedido.created_at | dateTimeToHours }}</strong>
-              </v-col>
-              <v-col>
-                <strong>{{ calculaValorPedido(pedido) | formatMoeda }}</strong>
-              </v-col>
+
+    <v-timeline align-top dense>
+      <template v-for="(pedido, index) in pedidos">
+        <v-timeline-item
+          :color="colorStatusPedido(pedido.status_pedido_id)"
+          :key="index"
+          small
+        >
+          <template x-small v-slot:icon>
+            <!-- <v-icon>mdi-account</v-icon> -->
+          </template>
+          <v-row class="pt-0">
+            <v-col cols="3">
+              <strong>n° {{ pedido.id }}</strong>
+            </v-col>
+            <v-col>
+              <strong>{{ pedido.created_at | dateTimeToHours }}</strong>
+            </v-col>
+            <v-col>
+              <strong>{{ calculaValorPedido(pedido) | formatMoeda }}</strong>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+        <template v-for="(produtoPedido, index) in pedido.produtos_pedidos">
+          <v-timeline-item hide-dot :key="index">
+            <v-row class="pb-4 pt-4">
+              <v-badge
+                color="green"
+                :value="produtoPedido.produtos_adicionais_pedidos.length"
+                :content="`+${produtoPedido.produtos_adicionais_pedidos.length}`"
+              >
+                <v-chip small class="mr-1"
+                  >{{ produtoPedido.quantidade }} x</v-chip
+                >
+                <div class="d-inline">
+                  {{ produtoPedido.produto.nome }}
+                </div>
+              </v-badge>
             </v-row>
           </v-timeline-item>
-          <template v-for="(produtoPedido, index) in pedido.produtos_pedidos">
-            <v-timeline-item hide-dot :key="index">
-              <v-row class="pb-4 pt-4">
-                <v-badge
-                  color="green"
-                  :value="produtoPedido.produtos_adicionais_pedidos.length"
-                  :content="`+${produtoPedido.produtos_adicionais_pedidos.length}`"
-                >
-                  <v-chip small class="mr-1"
-                    >{{ produtoPedido.quantidade }} x</v-chip
-                  >
-                  <div class="d-inline">
-                    {{ produtoPedido.produto.nome }}
-                  </div>
-                </v-badge>
-              </v-row>
-            </v-timeline-item>
-          </template>
         </template>
-      </v-timeline>
-    </v-card-text>
+      </template>
+    </v-timeline>
   </div>
 </template>
 <script>
